4b7ca1148db52555bc7381c818c3cb5bd710f9c6
Yukie-Billal <yukiembillal01@gmail.com>
2024-09-26 22:47:21 +0700

    ui: navbar

--- index.php
+++ index.php
@@ -9,41 +9,36 @@
 </head>
 
 <body>
-   <div class="container-fluid">
-      <nav class="navbar navbar-expand-lg bg-dark fixed-top">
-         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+   <div class="container-fluid p-0">
+      <nav class="navbar navbar-expand-lg bg-primary">
+         <div class="container-fluid px-4 py-1">
+            <h1 class="navbar-brand fs-4" href="#">LOGO</h1>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
+               <ul class="navbar-nav">
                   <li class="nav-item">
                      <a class="nav-link active" aria-current="page" href="#">Home</a>
                   </li>
                   <li class="nav-item">
-                     <a class="nav-link" href="#">Link</a>
-                  </li>
-                  <li class="nav-item dropdown">
-                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                     </a>
-                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li>
-                           <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                     </ul>
+                     <a class="nav-link" href="#">About Us</a>
                   </li>
                   <li class="nav-item">
-                     <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                     <a class="nav-link" href="#">Project</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="#">Career</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="#">Contact</a>
                   </li>
                </ul>
             </div>
          </div>
       </nav>
+
+      <h1>INI CONTENT</h1>
    </div>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
