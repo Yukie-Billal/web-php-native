96346a5da3a2d3e06c14163f49f231a775d71454
Yukie-Billal <yukiembillal01@gmail.com>
2024-09-29 22:38:39 +0700

    ui: carousel

--- index.php
+++ index.php
@@ -38,7 +38,29 @@
          </div>
       </nav>
 
-      <h1>INI CONTENT</h1>
+      <div class="carousel-wrapper" style="height: 600px; overflow: hidden;">
+         <div id="carouselExample" class="carousel slide h-100" data-bs-ride="carousel">
+            <div class="carousel-inner">
+               <div class="carousel-item active">
+                  <img src="./background.jpg" class="d-block w-100" alt="...">
+               </div>
+               <div class="carousel-item">
+                  <img src="./background.jpg" class="d-block w-100" alt="...">
+               </div>
+               <div class="carousel-item">
+                  <img src="./background.jpg" class="d-block w-100" alt="...">
+               </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+               <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+               <span class="carousel-control-next-icon" aria-hidden="true"></span>
+               <span class="visually-hidden">Next</span>
+            </button>
+         </div>
+      </div>
    </div>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
