a7e521ccca87c51f6f2a73b7a8ab7f1013fc08f5
Yukie-Billal <yukiembillal01@gmail.com>
2024-10-04 11:38:21 +0700

    feat: halaman baru, dan pemisahan navbar

--- index.php
+++ index.php
@@ -11,33 +11,12 @@
 
 <body>
    <div class="container-fluid p-0">
-      <nav class="navbar navbar-expand-lg">
-         <div class="container-fluid px-4 py-1">
-            <h1 class="navbar-brand fs-4" href="#">LOGO</h1>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-               <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
-               <ul class="navbar-nav">
-                  <li class="nav-item">
-                     <a class="nav-link text-white active" aria-current="page" href="#">Home</a>
-                  </li>
-                  <li class="nav-item">
-                     <a class="nav-link text-white" href="#">About Us</a>
-                  </li>
-                  <li class="nav-item">
-                     <a class="nav-link text-white" href="#">Project</a>
-                  </li>
-                  <li class="nav-item">
-                     <a class="nav-link text-white" href="#">Career</a>
-                  </li>
-                  <li class="nav-item">
-                     <a class="nav-link text-white" href="#">Contact</a>
-                  </li>
-               </ul>
-            </div>
-         </div>
-      </nav>
+
+      <?php
+
+      include "./navbar.php"
+
+      ?>
 
       <div class="carousel-wrapper" style="height: 600px; overflow: hidden;">
          <div id="carouselExample" class="carousel slide h-100" data-bs-ride="carousel">
---
+++ navbar.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg">
+   <div class="container-fluid px-4 py-1">
+      <h1 class="navbar-brand fs-4" href="#">LOGO</h1>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse d-flex justify-content-end" id="navbar-menu">
+         <ul class="navbar-nav">
+            <li class="nav-item">
+               <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" href="#">About Us</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" href="#">Project</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" href="#">Career</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" href="#">Contact</a>
+            </li>
+         </ul>
+      </div>
+   </div>
+</nav>
