c6c64e1d755ae276b048ef0581546b8cc14cbcae
Yukie-Billal <yukiembillal01@gmail.com>
2024-10-06 23:39:29 +0700

    ui: layout header

--- product.php
+++ product.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Product</title>
-</head>
+<?php
+include "./header.php";
+?>
 
 <body>
    <div class="container-fluid p-0">
