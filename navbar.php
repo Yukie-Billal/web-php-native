115ae7bff89526cd9c3bc5927e84fb0a2ab36897
Yukie-Billal <yukiembillal01@gmail.com>
2024-10-06 22:24:56 +0700

    feat: linked navbar to page

--- navbar.php
+++ navbar.php
@@ -1,3 +1,9 @@
+<?php
+
+$url_path = $_SERVER["REQUEST_URI"];
+
+?>
+
 <nav class="navbar navbar-expand-lg">
    <div class="container-fluid px-4 py-1">
       <h1 class="navbar-brand fs-4" href="#">LOGO</h1>
@@ -7,19 +13,19 @@
       <div class="collapse navbar-collapse d-flex justify-content-end" id="navbar-menu">
          <ul class="navbar-nav">
             <li class="nav-item">
-               <a class="nav-link active" aria-current="page" href="#">Home</a>
+               <a class="nav-link" aria-current="page" href="./index.php">Home</a>
             </li>
             <li class="nav-item">
-               <a class="nav-link" href="#">About Us</a>
+               <a class="nav-link" href="./index.php#about-section">About Us</a>
             </li>
             <li class="nav-item">
-               <a class="nav-link" href="#">Product</a>
+               <a class="nav-link <?= $url_path == "/product.php" ? "active" : "" ?>" href="./product.php">Product</a>
             </li>
             <li class="nav-item">
                <a class="nav-link" href="#">Documentation</a>
             </li>
             <li class="nav-item">
-               <a class="nav-link" href="#">Contact</a>
+               <a class="nav-link <?= $url_path == "/product.php" ? "active" : "" ?>" href="./contact.php">Contact</a>
             </li>
          </ul>
       </div>
