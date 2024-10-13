e377237d2b9f7266e6f5730d18029c7b3910c134
Yukie-Billal <yukiembillal01@gmail.com>
2024-10-13 23:17:12 +0700

    ui: hero-section produk page

--- product.php
+++ product.php
@@ -9,7 +9,13 @@ include "./header.php";
       include "./navbar.php"
 
       ?>
-      <h1>Ini halaman untuk project</h1>
+      <div id="hero-section" class="w-100 d-flex justify-content-center align-items-center" style="height: 100vh;">
+         <img src="./background.jpg" alt="" width="100%" style="height: 100%;" class="position-absolute">
+         <div class="position-relative w-25 d-flex justify-content-center align-items-center flex-column">
+            <h2 class="border-bottom border-white text-center text-white fs-1 w-100 fw-semibold" style="text-shadow: 1px 1px 1px #1a1a1a;">Produk Kami</h2>
+            <a class="btn btn-primary" style="width: fit-content;" href="#contact-section">SELENGKAPNYA</a>
+         </div>
+      </div>
    </div>
 </body>
 
