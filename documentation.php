b1ad9968667ae9027b0f46a0349d6f47815acd6b
Yukie-Billal <yukiembillal01@gmail.com>
2024-10-26 20:51:58 +0700

    ui: documentation page

--- documentation.php
+++ documentation.php
@@ -6,10 +6,21 @@ include "./header.php";
    <div class="container-fluid p-0">
       <?php
 
-      include "./navbar.php"
+      include "./navbar.php";
 
       ?>
-      <h1>Ini halaman untuk project</h1>
+      <div id="hero-section" class="w-100 d-flex justify-content-center align-items-center" style="height: 100vh;">
+         <img src="./background.jpg" alt="" width="100%" style="height: 100%;" class="position-absolute">
+         <div class="position-relative w-25 d-flex justify-content-center align-items-center flex-column">
+            <h2 class="border-bottom border-white text-center text-white fs-1 w-100 fw-semibold" style="text-shadow: 1px 1px 1px #1a1a1a;">Dokumentasi</h2>
+            <a class="btn btn-primary" style="width: fit-content;" href="#documentation-section">SELENGKAPNYA</a>
+         </div>
+      </div>
+      <div id="documentation-section">
+      </div>
+      <?php
+      include "./footer.php";
+      ?>
    </div>
 </body>
 
