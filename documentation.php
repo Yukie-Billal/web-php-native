ad9feabc32c2c5fe1a7e10dfcd72dcbb0042dd8c
Yukie-Billal <yukiembillal01@gmail.com>
2024-10-27 14:31:39 +0700

    ui: carousel bs

--- documentation.php
+++ documentation.php
@@ -17,6 +17,37 @@ include "./header.php";
          </div>
       </div>
       <div id="documentation-section">
+         <div class="row">
+            <div class="col-12">
+               <h6 class="text-center fs-2 fw-semibold">Dokumentasi Kegiatan</h6>
+               <h6 class="text-center fs-4 fw-medium">Lihat bagaimana aktifitas kami berlangsung</h6>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-12">
+               <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false">
+                  <div class="carousel-inner">
+                     <div class="carousel-item active">
+                        <img src="..." class="d-block w-100" alt="...">
+                     </div>
+                     <div class="carousel-item">
+                        <img src="..." class="d-block w-100" alt="...">
+                     </div>
+                     <div class="carousel-item">
+                        <img src="..." class="d-block w-100" alt="...">
+                     </div>
+                  </div>
+                  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                     <span class="visually-hidden">Previous</span>
+                  </button>
+                  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                     <span class="visually-hidden">Next</span>
+                  </button>
+               </div>
+            </div>
+         </div>
       </div>
       <?php
       include "./footer.php";
