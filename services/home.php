86ca322aab1eb8926d5c4a45c09fa54f99f16062
Yukie-Billal <yukiembillal01@gmail.com>
2024-11-08 11:06:38 +0700

    feat: home feature seeder

--- services/home.php
+++ services/home.php
@@ -1,6 +1,24 @@
 <?php
 
 include_once __DIR__ . "/select.php";
+include_once __DIR__ . "/../sources/home.php";
+
+function feature_seeder($connection, $datas) : void {
+   $feature_count = mysqli_query($connection, "SELECT COUNT(*) AS count FROM home_feature");
+   if ($feature_count) {
+      $count = $feature_count->fetch_column();
+      if ($count <= 0) {
+         foreach ($datas as $key => $data) {
+            $title = $data['title'];
+            $description = $data['description'];
+            $icon_path = $data['icon_path'];
+            mysqli_query($connection, "INSERT INTO home_feature (id, title, description, icon_path) VALUES (NULL, '$title', '$description', '$icon_path')");
+         }
+      }
+   }
+}
+
+feature_seeder($connection, $feature_datas);
 
 function get_features($connection) {
    return select($connection, "SELECT * FROM home_feature");
---
+++ sources/home.php
@@ -0,0 +1,20 @@
+<?php
+
+$feature_datas = [
+   [
+      "title" => "Professional Team",
+      "description" => "We have our professional team to manage our project",
+      "icon_path" => "",
+   ],
+   [
+      "title" => "We deliver Quality",
+      "description" => "Quality is our services priority",
+      "icon_path" => "",
+   ],
+   [
+      "title" => "Competitive",
+      "description" => "We provide competitive price, schedule and method",
+      "icon_path" => "",
+   ],
+];
+
